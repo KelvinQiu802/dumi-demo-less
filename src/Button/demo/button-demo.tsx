400310e336e60fddf00e612d36aae79cafa2edfb
KelvinQiu <kelvinqiu802@outlook.com>
2023-01-17 19:07:51 +0800

    feat: change button style

--- src/Button/demo/button-demo.tsx
+++ src/Button/demo/button-demo.tsx
@@ -4,12 +4,12 @@ import Button from '../index';
 
 export default () => {
   return (
-    <>
+    <div style={{ display: 'flex', gap: '30px' }}>
       <Button text="Hello" disabled={true} />
       <GlobalTheme isDarkMode={true}>
         <Button text="World" />
       </GlobalTheme>
       <Button text="Hello" style={{ color: 'red' }} />
-    </>
+    </div>
   );
 };
